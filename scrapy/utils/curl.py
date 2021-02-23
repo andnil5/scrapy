4f3a01e45bf4b7a2bab975ea4cea577566e6d310
andnil5 <andnil5@kth.se>
2021-02-23 15:28:54 +0100

    feat: add branch flag setters in `curl_to_request_kwargs`, issue #4

--- scrapy/utils/curl.py
+++ scrapy/utils/curl.py
@@ -6,7 +6,7 @@ from urllib.parse import urlparse
 
 from w3lib.http import basic_auth_header
 
-coverage_curl_to_request_kwargs = [False]*10
+coverage_curl_to_request_kwargs = [False]*25
 
 class CurlParser(argparse.ArgumentParser):
     def error(self, message):
@@ -48,16 +48,24 @@ def curl_to_request_kwargs(curl_command, ignore_unknown_options=True):
     curl_args = split(curl_command)
 
     if curl_args[0] != 'curl':
+        coverage_curl_to_request_kwargs[0] = True
         raise ValueError('A curl command must start with "curl"')
+    else:
+        coverage_curl_to_request_kwargs[1] = True
 
     parsed_args, argv = curl_parser.parse_known_args(curl_args[1:])
 
     if argv:
+        coverage_curl_to_request_kwargs[2] = True
         msg = f'Unrecognized options: {", ".join(argv)}'
         if ignore_unknown_options:
+            coverage_curl_to_request_kwargs[3] = True
             warnings.warn(msg)
         else:
+            coverage_curl_to_request_kwargs[4] = True
             raise ValueError(msg)
+    else:
+        coverage_curl_to_request_kwargs[5] = True
 
     url = parsed_args.url
 
@@ -65,7 +73,16 @@ def curl_to_request_kwargs(curl_command, ignore_unknown_options=True):
     # needs the scheme to work
     parsed_url = urlparse(url)
     if not parsed_url.scheme:
+        coverage_curl_to_request_kwargs[6] = True
         url = 'http://' + url
+    else:
+        coverage_curl_to_request_kwargs[7] = True
+
+    # Only for branch coverage
+    if parsed_args.method:
+        coverage_curl_to_request_kwargs[8] = True
+    else:
+        coverage_curl_to_request_kwargs[9] = True
 
     method = parsed_args.method or 'GET'
 
@@ -73,29 +90,56 @@ def curl_to_request_kwargs(curl_command, ignore_unknown_options=True):
 
     headers = []
     cookies = {}
+
+    # Only for branch coverage
+    if parsed_args.headers:
+        coverage_curl_to_request_kwargs[10] = True
+    else:
+        coverage_curl_to_request_kwargs[11] = True
+
     for header in parsed_args.headers or ():
         name, val = header.split(':', 1)
         name = name.strip()
         val = val.strip()
         if name.title() == 'Cookie':
+            coverage_curl_to_request_kwargs[12] = True
             for name, morsel in SimpleCookie(val).items():
+                coverage_curl_to_request_kwargs[13] = True
                 cookies[name] = morsel.value
         else:
+            coverage_curl_to_request_kwargs[14] = True
             headers.append((name, val))
 
     if parsed_args.auth:
+        coverage_curl_to_request_kwargs[15] = True
         user, password = parsed_args.auth.split(':', 1)
         headers.append(('Authorization', basic_auth_header(user, password)))
+    else:
+        coverage_curl_to_request_kwargs[16] = True
 
     if headers:
+        coverage_curl_to_request_kwargs[17] = True
         result['headers'] = headers
+    else:
+        coverage_curl_to_request_kwargs[18] = True
+
     if cookies:
+        coverage_curl_to_request_kwargs[19] = True
         result['cookies'] = cookies
+    else:
+        coverage_curl_to_request_kwargs[20] = True
+
     if parsed_args.data:
+        coverage_curl_to_request_kwargs[21] = True
         result['body'] = parsed_args.data
         if not parsed_args.method:
+            coverage_curl_to_request_kwargs[22] = True
             # if the "data" is specified but the "method" is not specified,
             # the default method is 'POST'
             result['method'] = 'POST'
+        else:
+            coverage_curl_to_request_kwargs[23] = True
+    else:
+        coverage_curl_to_request_kwargs[24] = True
 
     return result
