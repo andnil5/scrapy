84f3f5a3520309aaf7c1edda1e8a24339cb9564a
Maja Tennander <majate@kth.se>
2021-03-04 16:01:57 +0100

    style: fix flake8 warnings

--- scrapy/downloadermiddlewares/httpequivencoding.py
+++ scrapy/downloadermiddlewares/httpequivencoding.py
@@ -1,6 +1,7 @@
 from scrapy.exceptions import NotConfigured
 from scrapy.http import TextResponse
 
+
 class HttpEquivEncodingMiddleware:
     """
     Obey a ``http-equiv`` header for ``Content-Type`` encoding when the HTTP
@@ -16,10 +17,10 @@ class HttpEquivEncodingMiddleware:
     def process_response(self, request, response, spider):
 
         if isinstance(response, TextResponse) \
-            and response._encoding is None \
-            and response._headers_encoding is not None \
-            and response._body_declared_encoding() is not None \
-            and response._headers_encoding() != response._body_declared_encoding():
+                and response._encoding is None \
+                and response._headers_encoding() is not None \
+                and response._body_declared_encoding() is not None \
+                and response._headers_encoding() != response._body_declared_encoding():
             return response.replace(encoding=response._body_declared_encoding())
-            
+
         return response
