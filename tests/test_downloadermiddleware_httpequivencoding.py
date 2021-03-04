a9e34a3193cf8986d11167a0743baad6ff0b742d
andnil5 <andnil5@kth.se>
2021-03-04 16:54:54 +0100

    docs: add documentation for HttpEquivEncodingMiddleware tests

--- tests/test_downloadermiddleware_httpequivencoding.py
+++ tests/test_downloadermiddleware_httpequivencoding.py
@@ -9,6 +9,9 @@ from scrapy.exceptions import NotConfigured
 class HttpEquivEncodingMiddlewareTest(unittest.TestCase):
 
     def _get_meta_tag_body(self, charset):
+        """Creates a html body with a http-equiv attribute declaring
+           a specific charset.
+        """
         body = """
             <html><head>
                 <meta http-equiv="Content-Type" content="text/html; charset={}" />
@@ -16,21 +19,31 @@ class HttpEquivEncodingMiddlewareTest(unittest.TestCase):
         return body.encode(charset)
 
     def _get_header(self, charset):
+        """Creates a header dictionary declaring a specific charset.
+        """
         return {"Content-type": ["text/html; charset={}".format(charset)]}
 
     def _get_mw_spider(self):
+        """Creates a spider and a HttpEquivEncodingMiddleware instance.
+        """
         crawler = get_crawler(settings_dict={'HTTPEQUIVENCODING_ENABLED': True})
         spider = crawler._create_spider('foo')
         mw = HttpEquivEncodingMiddleware.from_crawler(crawler)
         return mw, spider
 
     def _req_res(self, req_kwargs=None, res_kwargs=None):
+        """Creates a Request and a TextResponse instance with a
+           dummy url and kwargs as specified in the parameters.
+        """
         url = "http://example.com/"
         req = Request(url, **(req_kwargs or {}))
         res = TextResponse(url, request=req, **(res_kwargs or {}))
         return req, res
 
     def test_obey_body_encoding(self):
+        """Test that `process_response` obey body encoding if header 
+           and body disagree.
+        """
         mw, spider = self._get_mw_spider()
         req, res = self._req_res(res_kwargs={
             "body": self._get_meta_tag_body("windows-1252"),
@@ -40,6 +53,9 @@ class HttpEquivEncodingMiddlewareTest(unittest.TestCase):
         self.assertEqual(res2.encoding, "cp1252")
 
     def test_prioritise_encoding_argument(self):
+        """Test that `process_response` prioritise `_encoding`
+           if set.
+        """
         mw, spider = self._get_mw_spider()
         req, res = self._req_res(res_kwargs={
             "encoding": "utf-16",
@@ -51,6 +67,9 @@ class HttpEquivEncodingMiddlewareTest(unittest.TestCase):
         self.assertIs(res, res2)
 
     def test_no_headers_encoding(self):
+        """Test that `process_response` does not modify the response
+           if no charset is specified in the header.
+        """
         mw, spider = self._get_mw_spider()
         req, res = self._req_res(res_kwargs={
             "body": self._get_meta_tag_body("windows-1252"),
@@ -60,6 +79,9 @@ class HttpEquivEncodingMiddlewareTest(unittest.TestCase):
         self.assertIs(res, res2)
 
     def test_no_body_encoding(self):
+        """Test that `process_response` does not modify the response
+           if the body does not contain a charset declaration. 
+        """
         mw, spider = self._get_mw_spider()
         req, res = self._req_res(res_kwargs={
             "body": b'<html> <head></head> </html>',
@@ -70,6 +92,9 @@ class HttpEquivEncodingMiddlewareTest(unittest.TestCase):
         self.assertIs(res, res2)
 
     def test_same_encoding(self):
+        """Test that `process_response` does not modify the response
+           if the body and header charset is equal.
+        """
         mw, spider = self._get_mw_spider()
         req, res = self._req_res(res_kwargs={
             "body": self._get_meta_tag_body("utf-8"),
@@ -80,6 +105,9 @@ class HttpEquivEncodingMiddlewareTest(unittest.TestCase):
         self.assertIs(res, res2)
 
     def test_not_text_response(self):
+        """Test that `process_response` does not modify the response
+           if the given response is not a TextResponse instance.
+        """
         mw, spider = self._get_mw_spider()
         url = "http://example.com/"
         req = Request(url)
